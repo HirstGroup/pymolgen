2d3c264bca93097298d0740bdacf9ccbb372417f
Bruno <bruno.falconepin@nottingham.ac.uk>
2022-09-01 15:56:00 +0100

    Fixed logp bug and added evaluator tests

--- evaluator.py
+++ evaluator.py
@@ -5,7 +5,8 @@ import random
 import time
 
 from fragment_builder import build_molecule
-from pymolgen.molecule_formats import molecule_to_smiles, molecule_to_inchi
+from pymolgen.molecule_formats import molecule_to_smiles, molecule_to_inchi, molecule_to_sdf
+from pymolgen.properties_pymolgen import *
 
 from rdkit import Chem
 from PP_ML_models.predictive_models.ml_model_gcnn_ens import Ensemble_Model_DC
@@ -61,16 +62,17 @@ if __name__ == '__main__':
     parser.add_argument('-o','--outfile_name', help='Output File Name',required=True)
     parser.add_argument('-n','--n_mol', type=int, help='Number of molecules to generate',required=True)
     parser.add_argument('-l','--log', help='Log file name',required=True)
+    parser.add_argument('--mpo', type=float, help='MPO threshold', required=True)
     parser.add_argument('--unique', action='store_true', help='Generate unique set of molecules', required=False)
     parser.add_argument('--rules', action='store_true', help='Use rules to filter', required=False)
     parser.add_argument('--rules_file', help='Rules file name for rules to filter', required=False)
     parser.add_argument('--filters', action='store_true', help='Use filters', required=False)
-    parser.add_argument('--restart', action='store_true', help='Restart generation from previous run')
+    parser.add_argument('--restart', action='store_true', help='Restart generation from previous run', required=False)
     parser.add_argument('--verbose', action='store_true', help='Verbose output', required=False)
-    parser.add_argument('--mw_check', action='store_true', help='MW filter in every fragment addition')
-    parser.add_argument('--no_numpy', action='store_true', help='Do not use numpy for fragment bond frequencies')
+    parser.add_argument('--mw_check', action='store_true', help='MW filter in every fragment addition', required=False)
+    parser.add_argument('--no_numpy', action='store_true', help='Do not use numpy for fragment bond frequencies', required=False)
     parser.add_argument('--batch_size', type=int, help='Batch size for rules', required=False)
-    parser.add_argument('--mpo', type=float, help='MPO threshold', required=True)
+    parser.add_argument('--no_time', action='store_true', help='Do not print time interval in csv file', required=False)
 
     args = parser.parse_args()
 
@@ -87,8 +89,13 @@ if __name__ == '__main__':
     # Run prediction model once to initialise:
     _ = pIC50_pred_model.predict('C')[0]
 
-    out = open(args.log, 'w')
-    out.write('inchi;pIC50_pred;mpo;pfi;logp;n_aromatic;time\n')
+    with open(args.log, 'w') as out:
+        if args.no_time:
+            out.write('inchi;smi;pIC50_pred;mpo;pfi;psa;logp;n_aromatic;n_rot_bonds;n_chiral;h_acc;mw;h_don\n')
+        else:
+            out.write('inchi;smi;pIC50_pred;mpo;pfi;psa;logp;n_aromatic;n_rot_bonds;n_chiral;h_acc;mw;h_don;interval_time\n')
+
+    outfile = open(args.outfile_name, 'w')
 
     start_time = time.time()
     current_time = start_time
@@ -108,7 +115,7 @@ if __name__ == '__main__':
                 oemol = oechem.OEGraphMol()
                 oechem.OESmilesToMol(oemol, smi)
                 oechem.OEAddExplicitHydrogens(oemol)
-                
+
                 logp = mp.OEGetXLogP(oemol, atomxlogps=None)
 
                 n_aromatic = Chem.rdMolDescriptors.CalcNumAromaticRings(Chem.MolFromSmiles(smi))
@@ -129,10 +136,31 @@ if __name__ == '__main__':
                 current_time = time.time() - start_time
                 interval_time = current_time - previous_time
 
-                print(f'EVALUATOR {n} {mw:.2f} {smi} {interval_time:.2f}')
+                print(f'EVALUATOR {n} {mw:.2f} {inchi} {interval_time:.2f}')
 
-                out.write('{};{};{};{};{};{};{:.2f}\n'.format(inchi, pIC50_pred, mpo, pfi, logp, n_aromatic, interval_time))
-                out.flush()
+                psa = mp.OEGet2dPSA(oemol,atomPSA = None)
+
+                n_rot_bonds = num_rot_bond(oemol)
+
+                n_chiral = num_chiral_centres(oemol)
+
+                h_acc = num_lipinsky_acceptors(oemol)
+
+                h_don = num_lipinsky_donors(oemol)
+
+                with open(args.log, 'a') as out:
+                    if args.no_time:
+                        out.write(f'{inchi};{smi};{pIC50_pred};{mpo};{pfi};{psa};{logp};{n_aromatic};{n_rot_bonds};{n_chiral};{h_acc};{mw:.2f};{h_don}\n')
+                    else:
+                        out.write(f'{inchi};{smi};{pIC50_pred};{mpo};{pfi};{psa};{logp};{n_aromatic};{n_rot_bonds};{n_chiral};{h_acc};{mw:.2f};{h_don};{interval_time:.2f}\n')
+
+
+                lines = molecule_to_sdf(mol)
+
+                with open(args.outfile_name, 'a') as outfile:
+                    for line in lines:
+                        outfile.write(line)
+                    outfile.write('$$$$\n')
 
                 if n == args.n_mol:
                     sys.exit('Normal termination')

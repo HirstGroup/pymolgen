953aec06cf3f1768c0847c1acc0d54abb018ba22
Bruno <bruno.falconepin@nottingham.ac.uk>
2022-09-15 16:22:25 +0100

    Batch_size x10 cpu

--- test/test_fragment_builder.py
+++ test/test_fragment_builder.py
@@ -87,9 +87,11 @@ def test_get_fragment_bond_frequencies_np():
 
 def test_fragment_builder(cpu):
 
+    batch_size = cpu * 10
+
     random.seed(100)
 
-    fragment_builder(fragments_sdf='../datasets/database1000/fragments1000.sdf', fragments_txt='../datasets/database1000/fragments1000.txt', frequencies_txt='../datasets/database1000/frequencies1000.txt', parent_file='../datasets/database1000/phenylisoxazole.sdf', parent_fragment_file_list=['../datasets/database1000/benzene.sdf','../datasets/database1000/benzene.sdf'], parent_mapping_1=[16,0,15,0], remove_hydrogens=[20,21], remove_hydrogens_parent_fragment=[11,11],outfile_name='outputs/fragment_builder.sdf', n_mol=10000, unique=False, rules=False, rules_file=None, filters=False, restart=False, verbose=True, mw_check=True, use_numpy=True, batch_size=80, cpu=cpu)
+    fragment_builder(fragments_sdf='../datasets/database1000/fragments1000.sdf', fragments_txt='../datasets/database1000/fragments1000.txt', frequencies_txt='../datasets/database1000/frequencies1000.txt', parent_file='../datasets/database1000/phenylisoxazole.sdf', parent_fragment_file_list=['../datasets/database1000/benzene.sdf','../datasets/database1000/benzene.sdf'], parent_mapping_1=[16,0,15,0], remove_hydrogens=[20,21], remove_hydrogens_parent_fragment=[11,11],outfile_name='outputs/fragment_builder.sdf', n_mol=10000, unique=False, rules=False, rules_file=None, filters=False, restart=False, verbose=True, mw_check=True, use_numpy=True, batch_size=batch_size, cpu=cpu)
 
 cpu = int(sys.argv[1])
 

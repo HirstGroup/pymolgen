1335f318e94bad7e33f2ac86d2376b7a46660a7c
Bruno Falcone <bnfalcone@yahoo.es>
2022-10-11 16:34:19 +0100

    Calculator

---
+++ calculator.py
@@ -0,0 +1,76 @@
+import sys,os
+import argparse
+import numpy as np
+import random
+import time
+
+from pymolgen.molecule_formats import molecule_to_smiles, molecule_to_inchi, molecule_to_sdf
+from pymolgen.properties_pymolgen import *
+
+from rdkit import Chem
+from PP_ML_models.predictive_models.ml_model_gcnn_ens import Ensemble_Model_DC
+
+# Import Openeye Modules
+from openeye import oechem
+from openeye import oemolprop as mp
+
+from os.path import expanduser
+home = expanduser("~")
+
+# Add path so the predictive_models and properties modules can be found
+head_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(head_path)
+sys.path.append(home + '/PP_ML_models')
+
+def oracle(mol):
+
+    mw = mol.molecular_weight()
+
+    if mw < 500:
+        return True
+
+# Calculate logP:
+def oeLogP(smi):
+    mol = oechem.OEGraphMol()
+    if not oechem.OESmilesToMol(mol, smi):
+        print('ERROR: {}'.format(smi))
+    else:
+        logp = mp.OEGetXLogP(mol, atomxlogps=None)
+    return logp
+
+def oeLogP_oemol(oemol):
+    mol = oechem.OEGraphMol()
+    if not oechem.OESmilesToMol(mol, smi):
+        print('ERROR: {}'.format(smi))
+    else:
+        logp = mp.OEGetXLogP(mol, atomxlogps=None)
+    return logp
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Pymolgen molecular generator from fragments')
+    parser.add_argument('-i','--input', help='Input file of inchi',required=True)
+    parser.add_argument('-o','--output', help='Output file',required=True)
+
+    pIC50_pred_model = Ensemble_Model_DC(home + '/PP_ML_models/pIC50.pk')
+    print(pIC50_pred_model.info)
+    print(pIC50_pred_model.version)
+    # Run prediction model once to initialise:
+    _ = pIC50_pred_model.predict('C')[0]
+
+    outfile = open(args.outfile_name, 'w')
+
+    infile = open(args.input)
+
+    for line in infile:
+
+        inchi = line.strip('\n')
+
+        rdmol = Chem.MolFromInchi(inchi)
+
+        smi = Chem.MolToSmiles(rdmol)
+
+    sys.exit('Normal termination')
+
+            
+

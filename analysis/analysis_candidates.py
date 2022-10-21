122d1eb623ff0fbc23945f3df3bbcfbbc1affe8e
Bruno <bruno.falconepin@nottingham.ac.uk>
2022-10-21 14:54:35 +0100

    Intermediate write

--- analysis/analysis_candidates.py
+++ analysis/analysis_candidates.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import argparse
 
+from functools import partial
+print = partial(print, flush=True)
+
 parser = argparse.ArgumentParser(description='Analyse candidates inchi file')
 parser.add_argument('-i', '--input', nargs='+', help='Space-separated list of inchi input files',required=True)
 parser.add_argument('-o', '--output', help='Output file',required=True)
@@ -26,9 +29,11 @@ for input in args.input:
         else:
             d[a] += 1
 
+    d = {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)}
 
-d = {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)}
+    write(d, args.output)
 
-with open(args.output, 'w') as outfile:
-    for key, val in d.items():
-        outfile.write('%s %s\n' %(key, val))
+def write(d, outfile_name)
+    with open(outfile_name, 'w') as outfile:
+        for key, val in d.items():
+            outfile.write('%s %s\n' %(key, val))

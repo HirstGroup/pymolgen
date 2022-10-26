50ea585f2b9a0820de0378c7248c7d30d8669e29
Bruno Falcone <bnfalcone@yahoo.es>
2022-10-26 12:35:58 +0100

    Sort

--- utils/sort_df.py
+++ utils/sort_df.py
@@ -9,6 +9,10 @@ df = pd.read_csv(input, sep=';')
 
 print(df.head())
 
+df = df.loc[df['rules_filter'] == True]
+
 df.sort_values('mpo', inplace=True)
+
+df = df.loc[df['mpo'] <= -6.5]
 
 df.to_csv(output, sep=';', index=False)

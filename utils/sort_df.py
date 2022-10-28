0d3b5ccd943b38cc41a9761ab0935fe8e4457a00
Bruno <bruno.falconepin@nottingham.ac.uk>
2022-10-28 11:55:56 +0100

    MPO filter

--- utils/sort_df.py
+++ utils/sort_df.py
@@ -1,16 +1,24 @@
 import pandas as pd
 import numpy as np
 import sys,os 
+import argparse
 
-input = sys.argv[1]
-output = sys.argv[2]
+parser = argparse.ArgumentParser(description='Pymolgen molecular generator from fragments')
+parser.add_argument('-i','--input', help='Input file of inchi',required=True)
+parser.add_argument('-o','--output', help='Output file',required=True)
+parser.add_argument('--mpo', type=float, help='MPO threshold',required=False)
 
-print(input, output)
+args = parser.parse_args()
 
-df = pd.read_csv(input, sep=';')
+print(args.input, args.output)
+
+df = pd.read_csv(args.input, sep=';')
 
 df = df.loc[df['rules_filter'] == True]
 
 df.sort_values('mpo', inplace=True)
 
-df.to_csv(output, sep=';', index=False)
+if args.mpo is not None:
+	df = df.loc[df['mpo'] <= args.mpo]
+
+df.to_csv(args.output, sep=';', index=False)

d8af0f3cdbaf47de56d3c6824383b26ef7a58121
Bruno Falcone <bnfalcone@yahoo.es>
2023-02-24 14:32:14 +0000

    Fragment Graph Class built

--- fragment_graph.py
+++ fragment_graph.py
@@ -1,14 +1,67 @@
+from typing import Tuple, Dict, List
+
+class FragmentGraphNode:
+
+	def __init__(self, attachment_points: List[int]):
+		self._attachment_points = attachment_points
+
+	@property
+	def attachment_points(self):
+		return list(self._attachment_points)
+
 class FragmentGraph:
 
 	def __init__(self):
+		self._fragments: Dict[int, FragmentGraphNode] = dict()
+		self._bonds: List[Tuple(int, int, int, int)] = []
+		self._attachment_point_list = []
+		self._free_valence_points = []
 
-	    self.fragments = [-1] #list of fragment indexes from database, parent fragment shown as -1
-	    self.frag_edge_list = [] #list of bonds between fragments
-	    self.frag_free_valence_list = [[]] #list of free valence points of each fragment		
+	@property
+	def fragments(self):
+		return dict(self._fragments)
 
-	def add_fragment(self, fragment):
+	@property
+	def bonds(self):
+		return list(self._bonds)
 
-		self.fragments.append(fragment)
+	def add_fragment(self, id: int, attachment_points: List[int]):
+		if len(attachment_points) < 1:
+			raise ValueError("A fragment must have at least 1 attachment point")
+		self._fragments[id] = FragmentGraphNode(attachment_points)
+		self._attachment_point_list.append(attachment_points)
+		self._free_valence_points.append(attachment_points)
 
-		pass
+	def add_bond(self, fragment_from: int, fragment_to: int, attach_from: int, attach_to: int):
 
+		if fragment_from > fragment_to:
+			# Ensure bonds are always stored 
+			# in acending-fragment order
+			tmp = fragment_to
+			fragment_to = fragment_from
+			fragment_from = tmp
+			tmp = attach_to
+			attach_to = attach_from
+			attach_from = tmp
+
+		# Check acending order and that fragments don't bond to themselves
+		assert fragment_from < fragment_to
+
+		# Check bond is between existing fragments
+		assert 0 <= fragment_from < len(self._fragments)
+		assert 0 <= fragment_to < len(self._fragments)
+
+		# Check attachment points are valid
+		assert attach_from in self._fragments[fragment_from].attachment_points
+		assert attach_to in self._fragments[fragment_to].attachment_points
+		assert attach_from in self._attachment_point_list[fragment_from]
+		assert attach_to in self._attachment_point_list[fragment_to]
+
+		# Check that the attachment points are free
+		assert attach_from in self._free_valence_points[fragment_from]
+		assert attach_to in self._free_valence_points[fragment_to]
+
+		# Make bond
+		self._bonds.append((fragment_from, fragment_to, attach_from, attach_to))
+		self._free_valence_points[fragment_from].remove(attach_from)
+		self._free_valence_points[fragment_to].remove(attach_to)
--- test/test_fragment_graph.py
+++ test/test_fragment_graph.py
@@ -5,6 +5,56 @@ def test_create_fragment_graph():
 
 def test_single_fragment_graph():
 	f = FragmentGraph()
-	f.add_fragment(0)
+	f.add_fragment(10, [1])
+	assert len(f.fragments) == 1
 
-	assert 0 in f.fragments
+def test_add_incorrect_fragment_fail():
+	f = FragmentGraph()
+	try:
+		# should fail because fragment doesn't have attachment points
+		f.add_fragment(0, [])
+	except ValueError:
+		return
+	assert False
+
+def test_add_bond():
+	f = FragmentGraph()
+	f.add_fragment(0, [0])
+	f.add_fragment(1, [0])
+	f.add_bond(0,1,0,0)
+	assert f.bonds == [(0,1,0,0)]
+
+def test_add_invalid_bond():
+	f = FragmentGraph()
+	try:
+		f.add_bond(0, 1, 0, 0)
+	except AssertionError:
+		return
+	assert False
+
+def test_add_invalid_bond_2():
+	f = FragmentGraph()
+	f.add_fragment(0, [1])
+	f.add_fragment(1, [1])
+	try:
+		# Should fail because fragment 1 doesn't have 1 as an attachment point
+		f.add_bond(0, 1, 0, 1)
+	except AssertionError:
+		return
+	assert False
+
+def test_add_invalid_bond_3():
+	f = FragmentGraph()
+	f.add_fragment(0, [1])
+	f.add_fragment(1, [0])
+	f.add_fragment(2, [1])
+		
+	f.add_bond(0, 1, 1, 0)
+
+	try:
+		# Should fail because fragment 1 has used up 0 as attachment point
+		f.add_bond(1,2,0,1)
+	except AssertionError:
+		return
+	assert False
+

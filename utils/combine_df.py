b41858e67d0e1414744bdfe85e7ec30a99290390
Bruno Falcone <bnfalcone@yahoo.es>
2023-03-29 13:46:05 +0100

    Added script to combine pandas csv files

---
+++ utils/combine_df.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import numpy as np
+import sys,os 
+import argparse
+
+parser = argparse.ArgumentParser(description='Combine data in dataframe format to single file')
+parser.add_argument('-i','--input', nargs='+', help='List of input file names',required=True)
+parser.add_argument('-o','--output', help='Output file',required=True)
+
+args = parser.parse_args()
+
+print(args.input, args.output)
+
+df = pd.read_csv(args.input[0], sep=';')
+
+for i in args.input[1:]:
+
+	df2 = pd.read_csv(i, sep=';')
+
+	df = pd.concat([df, df2])
+
+df.to_csv(args.output, sep=';', index=False)

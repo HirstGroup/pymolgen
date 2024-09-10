5f1436ba722c85279b1476b26b634b66d82dc518
Bruno Falcone <bnfalcone@yahoo.es>
2024-09-10 13:53:19 +0100

    Fixed test by setting batch_size to 1

--- evaluator.py
+++ evaluator.py
@@ -9,7 +9,7 @@ from pymolgen.molecule_formats import molecule_to_smiles, molecule_to_inchi, mol
 from pymolgen.properties_pymolgen import *
 
 from rdkit import Chem
-from PP_ML_models.predictive_models.ml_model_gcnn_ens import Ensemble_Model_DC
+from PP_ML_models.predictive_models.ml_model_gcnn import Ensemble_Model_DC
 
 # Import Openeye Modules
 from openeye import oechem
@@ -52,34 +52,34 @@ def oeLogP_oemol(oemol):
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Pymolgen molecular generator from fragments')
+    parser = argparse.ArgumentParser(description='Pymolgen molecular generator from fragments and evaluation of molecular properties')
 
     # required arguments
-    parser.add_argument('-a','--fragments_sdf', help='SDF file of fragments',required=True)
-    parser.add_argument('-f','--fragments_txt', help='List of fragments in TXT file',required=True)
-    parser.add_argument('-d','--frequencies_txt', help='Bond frequencies dictionary in txt file',required=True)
-    parser.add_argument('-p','--parent_file', help='Parent Structure File in SDF format',required=True)
-    parser.add_argument('-x','--parent_fragment_file_list', nargs='+', help='Parent Fragment Structure File list space-separated to search fragment database in SDF format',required=True)
-    parser.add_argument('--parent_mapping_1', nargs='+', type=int, help='Parent Fragment i dict list space-separated to search fragment database in SDF format',required=True)
-    parser.add_argument('-r','--remove_hydrogens', type=int, nargs='+', help='Space-separated hydrogen atoms that will be created as attachment points, numbered from 0',required=False)
-    parser.add_argument('-R','--remove_hydrogens_parent_fragment', type=int, nargs='+', help='Space-separated hydrogen atoms that will be created as attachment points for the parent fragment in database, numbered from 0',required=True)
+    parser.add_argument('-a','--fragments_sdf', help='SDF file of fragments', required=True)
+    parser.add_argument('-d','--frequencies_txt', help='Bond frequencies dictionary in txt file', required=True)
+    parser.add_argument('-f','--fragments_txt', help='List of fragments in TXT file', required=True)
+    parser.add_argument('-l','--log', help='Log file name', required=True)
+    parser.add_argument('-p','--parent_file', help='Parent Structure File in SDF format', required=True)
+    parser.add_argument('-R','--remove_hydrogens_parent_fragment', type=int, nargs='+', help='Space-separated hydrogen atoms that will be created as attachment points for the parent fragment in database, numbered from 0', required=True)
+    parser.add_argument('-x','--parent_fragment_file_list', nargs='+', help='Parent Fragment Structure File list space-separated to search fragment database in SDF format', required=True)
+    parser.add_argument('--mpo', type=float, help='MPO threshold', required=True)
+    parser.add_argument('--parent_mapping_1', nargs='+', type=int, help='Parent Fragment i dict list space-separated to search fragment database in SDF format', required=True)
 
     # optional arguments
-    parser.add_argument('-s','--seed', type=int, help='Seed for random number generator',required=False)
-    parser.add_argument('-o','--outfile_name', help='Output File Name',required=True)
-    parser.add_argument('-n','--n_mol', type=int, help='Number of molecules to generate',required=True)
-    parser.add_argument('-l','--log', help='Log file name',required=True)
-    parser.add_argument('--mpo', type=float, help='MPO threshold', required=True)
-    parser.add_argument('--unique', action='store_true', help='Generate unique set of molecules', required=False)
-    parser.add_argument('--rules', action='store_true', help='Use rules to filter', required=False)
-    parser.add_argument('--rules_file', help='Rules file name for rules to filter', required=False)
+    parser.add_argument('-n','--n_mol', type=int, help='Number of molecules to generate', required=True)
+    parser.add_argument('-o','--outfile_name', help='Output File Name', required=True)
+    parser.add_argument('-r','--remove_hydrogens', type=int, nargs='+', help='Space-separated hydrogen atoms that will be created as attachment points, numbered from 0', required=False)
+    parser.add_argument('-s','--seed', type=int, help='Seed for random number generator', required=False)
+    parser.add_argument('--batch_size', type=int, help='Batch size for rules', required=False)
     parser.add_argument('--filters', action='store_true', help='Use filters', required=False)
-    parser.add_argument('--restart', action='store_true', help='Restart generation from previous run', required=False)
-    parser.add_argument('--verbose', action='store_true', help='Verbose output', required=False)
     parser.add_argument('--mw_check', action='store_true', help='MW filter in every fragment addition', required=False)
     parser.add_argument('--no_numpy', action='store_true', help='Do not use numpy for fragment bond frequencies', required=False)
-    parser.add_argument('--batch_size', type=int, help='Batch size for rules', required=False)
     parser.add_argument('--no_time', action='store_true', help='Do not print time interval in csv file', required=False)
+    parser.add_argument('--rules', action='store_true', help='Use rules to filter', required=False)
+    parser.add_argument('--rules_file', help='Rules file name for rules to filter', required=False)
+    parser.add_argument('--restart', action='store_true', help='Restart generation from previous run', required=False)
+    parser.add_argument('--unique', action='store_true', help='Generate unique set of molecules', required=False)
+    parser.add_argument('--verbose', action='store_true', help='Verbose output', required=False)
 
     args = parser.parse_args()
 
@@ -102,7 +102,8 @@ if __name__ == '__main__':
         else:
             out.write('inchi;smi;pIC50_pred;mpo;pfi;psa;logp;n_aromatic;n_rot_bonds;n_chiral;h_acc;mw;h_don;interval_time\n')
 
-    outfile = open(args.outfile_name, 'w')
+    with open(args.outfile_name, 'w') as outfile:
+        print(f'Writing to {args.outfile_name}')
 
     start_time = time.time()
     current_time = start_time
--- test/test_evaluator.py
+++ test/test_evaluator.py
@@ -1,15 +1,24 @@
-import sys,os
 import filecmp
+import os
+import subprocess
+import sys
 
 path = os.path.dirname(os.path.realpath(__file__))
 
 def test_evaluator():
 
-	os.system(f'python ../evaluator.py -a ../datasets/database1000/fragments1000.sdf -f ../datasets/database1000/fragments1000.txt -d ../datasets/database1000/frequencies1000.txt -p ../datasets/database1000/phenylisoxazole.sdf -x ../datasets/database1000/benzene.sdf ../datasets/database1000/benzene.sdf -r 20 21 -R 11 11 -o outputs/evaluator1000.sdf -n 10 --parent_mapping 15 0 16 0 --batch_size 2 --log outputs/evaluator1000.csv --filters --rules --rules_file rules1000.smi --mpo -5.0 --unique --mw_check -s 100 --no_time')
+	os.chdir('outputs')
+
+	cmd = f'python ../../evaluator.py -a ../../datasets/database1000/fragments1000.sdf -f ../../datasets/database1000/fragments1000.txt -d ../../datasets/database1000/frequencies1000.txt -p ../../datasets/database1000/phenylisoxazole.sdf -x ../../datasets/database1000/benzene.sdf ../../datasets/database1000/benzene.sdf -r 20 21 -R 11 11 -o evaluator1000.sdf -n 2 --parent_mapping 15 0 16 0 --batch_size 1 --log evaluator1000.csv --filters --rules --rules_file rules1000.smi --mpo -5.0 --unique --mw_check -s 100 --no_time'
+
+	os.system(cmd)
+
+	#subprocess.run(cmd, check=True, shell=True)
+
+	os.chdir('../')
 
 	compare = filecmp.cmp('outputs/evaluator1000.sdf', '../datasets/database1000/evaluator1000.sdf', shallow=False)
 	assert compare is True
 
 	compare = filecmp.cmp('outputs/evaluator1000.csv', '../datasets/database1000/evaluator1000.csv', shallow=False)
 	assert compare is True
-

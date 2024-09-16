c18470db04d8c33a757e33fa5636e32ea09b7ee6
Bruno Falcone <bnfalcone@yahoo.es>
2024-09-16 14:10:36 +0100

    Added print progress

--- utils/find_missing_inchis.py
+++ utils/find_missing_inchis.py
@@ -13,25 +13,35 @@ inchis_to_check  = set()
 
 with open(args.input) as infile:
 
-    for line in infile:
+    for n, line in enumerate(infile):
 
         inchi = line.strip().split()[0]
 
         inchis_to_check.add(inchi)
 
+        if n % 1000000 == 0:
+            print('Number of inchis loaded', n)
+
 print('Number of inchis to check:', len(inchis_to_check))
 
 for aux in args.aux:
 
+    print('Auxfile', aux)
+
     with open(aux) as auxfile:
 
-        for line in auxfile:
+        for n, line in enumerate(auxfile):
 
             inchi = line.strip().split()[0]
 
             if inchi in inchis_to_check:
 
                 inchis_to_check.remove(inchi)
+
+            if n % 1000000 == 0:
+                print('Lines checked', n)
+
+        print('Number of inchis not found:', len(inchis_to_check))
 
 
 print('Number of inchis not found:', len(inchis_to_check))
